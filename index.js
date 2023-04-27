8e2468a58910062df68c749121c727e3afd3351b
ArmanChand <armanchand45@gmail.com>
2023-04-27 08:06:13 +0530

    Sentiment Analysis V1

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const Discord = require('discord.js');
+const bodyParser = require("body-parser");
+
+const { connectToMindsDBCloud, analyzeTextSentiment} = require("./dispatcher/mindsdb.js")
+
+
+const client = new Discord.Client({ intents: [
+  Discord.GatewayIntentBits.Guilds,
+  Discord.GatewayIntentBits.GuildMessages,
+  Discord.GatewayIntentBits.MessageContent
+]})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Discord bot is running!');
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith('!sentiment')) {
+    const query = message.content.slice(10).trim();
+    await connectToMindsDBCloud();
+    const response = await analyzeTextSentiment(query); 
+    console.log("result----->",response);
+    message.reply(JSON.stringify(response.rows[0]));
+  }
+});
+
+client.login(process.env.TOKEN);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express app is listening on port ${PORT}!`);
+});
